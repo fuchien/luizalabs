bc5fb0b205ded7abf35feb973941c52c74d41260
Chiien <fuchienhsu12@gmail.com>
2019-03-11 21:21:04 -0300

    Feat(routes.js) add new routes and multer for upload file

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 const express = require('express');
 
+const multerConfig = require('./config/multer');
+const upload = require('multer')(multerConfig);
+
+// CONTROLLERS
+const FileController = require('./app/controllers/FileController');
+
 const routes = express.Router();
 
-routes.get('/', (req, res) => res.send('Hello World'));
+routes.get('/', FileController.create);
+
+// FILE
+routes.post('/file', upload.single('file'), FileController.store);
 
 module.exports = routes;
