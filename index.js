b7a3e566aef41bcfebc45d3bf2c6b7374384a279
Chiien <fuchienhsu12@gmail.com>
2019-03-11 21:19:55 -0300

    Feat(index.js) change port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./src/server');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
