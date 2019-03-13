4986f3bb9227797ae76ff08f9ee23374abb8b685
Chiien <fuchienhsu12@gmail.com>
2019-03-13 18:59:36 -0300

    Feat(LogService.js) add Log model CRUD in log service file

---
+++ src/app/services/LogService.js
@@ -0,0 +1,64 @@
+const Log = require('../models/Log');
+
+class LogService {
+  create(log) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const logCreated = await Log.create({ ...log });
+        resolve(logCreated);
+      } catch (err) {
+        reject({
+          msg: 'Error to create',
+          err,
+        });
+      }
+    });
+  }
+
+  async show(gameId) {
+    return new Promise((resolve, reject) => {
+      try {
+        const log = await Log.findOne({ gameId });
+        resolve(log);
+      } catch (err) {
+        reject({
+          msg: 'Error to show game by id',
+          err,
+        });
+      }
+    })
+  }
+
+  async index() {
+    return new Promise((resolve, reject) => {
+      try {
+        const logs = await Log.find({}, null, {
+          sort: {
+            _id: 'asc',
+          },
+        });
+        resolve(logs);
+      } catch (err) {
+        reject({
+          msg: 'Error to get all game log',
+          err: err
+        })
+      }
+    })
+  }
+
+  async destroy() {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(await Log.deleteMany());
+      } catch (err) {
+        reject({
+          msg: 'Error to destroy all document',
+          err
+        })
+      }
+    })
+  }
+}
+
+module.exports = new LogService();
