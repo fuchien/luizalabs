d87d1b944e3fe1c07dc889ced2a0f2282aafcce1
Chiien <fuchienhsu12@gmail.com>
2019-03-18 14:21:44 -0300

    Feat(file.test.js) add file integration test

--- __tests__/integration/file.test.js
+++ __tests__/integration/file.test.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const request = require('supertest');
+const { promisify } = require('util');
+const { readFile } = require('fs');
+
+const readFilePromise = promisify(readFile);
 const path = require('path');
 const server = require('../../src/server');
 const FileService = require('../../src/app/services/FileService');
-const FileController = require('../../src/app/controllers/FileController');
 const LogService = require('../../src/app/services/LogService');
 
 describe('File', () => {
@@ -12,17 +15,27 @@ describe('File', () => {
     await LogService.destroy();
   });
 
-  it('should clear all file/database and show main', async () => {
-    // const filePath = path.resolve(__dirname, '..', '..', 'src', 'public', 'games.log');
-    const response = await request(server).get('/');
+  it('should save all file games log in database', async () => {
+    const filePath = path.resolve(__dirname, '..', '..', 'src', 'public', 'games.log');
+    const file = await readFilePromise(filePath, 'utf-8');
+    const response = await request(server)
+      .post('/file')
+      .send(file);
+    expect(response.status).toBe(200);
+    LogService.index = jest.fn();
+    LogService.index.mockImplementation(() => Promise.resolve(['', '']));
+    FileService.handleFile = jest.fn();
+    FileService.handleFile.mockImplementation(() => Promise.resolve(['', '']));
+    const logs = await FileService.handleFile(file);
+    expect(logs.length).toBe(2);
+  });
 
-    // expect(response).toBeInstanceOf(Object);
-    // await FileController.create();
+  it('should clear all file/database', async () => {
+    const response = await request(server).get('/');
+    expect(response.status).toBe(200);
     const uploads = path.resolve(__dirname, '..', '..', 'uploads');
     fs.readdir(uploads, (err, files) => {
       expect(files.length).toBe(0);
     });
-    const logs = await LogService.index();
-    expect(logs.length).toBe(0);
   });
 });
